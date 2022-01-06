a5495d954192412d40635f14958d61e39277a5a3
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-01-06 23:26:34 +0700

    update server

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use(cors())
 app.use('/api/auth', authRouter)
 app.use('/api/posts', postRouter)
 
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
