007bf396802f13a3c14d9c3339856688d01bea1e
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-01-17 21:56:00 +0700

    update homepage for server

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,7 +7,7 @@ const route = (app) => {
 
     app.use('/', (req, res) => {
         res.send(
-            "<div style='display:flex;justify-content:center;align-items:center;height:100vh;color:red;font-size:5rem'>Hi, I'm Tân!! ❤️❤️❤️</div>"
+            "<div style='display:flex;justify-content:center;align-items:center;height:100vh;color:red;font-size:5rem'><h1>Hi, I'm Tân!! ❤️❤️❤️</h1></div>"
         );
     });
 };
