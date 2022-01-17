8317f2c251fd6c628eacdb1a3bf356b5bf2445ae
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-01-17 21:11:30 +0700

    fix message

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,125 +1,131 @@
-require('dotenv').config()
-const express = require('express')
-const argon2 = require('argon2')
-const jwt = require('jsonwebtoken')
-const User = require('../models/User')
-const verifyToken = require('../middleware/auth')
-const router = express.Router()
+require('dotenv').config();
+const express = require('express');
+const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const verifyToken = require('../middleware/auth');
+const router = express.Router();
 
 // @route GET api/auth
 // @desc check if user is logged in
 // @access public
 router.get('/', verifyToken, async (req, res) => {
     try {
-        const user = await User.findById(req.userId).select('-password')
+        const user = await User.findById(req.userId).select('-password');
         if (!user) {
             return res.status(400).json({
                 success: false,
                 message: 'user not found'
-            })
+            });
         }
         res.json({
             success: true,
             user
-        })
+        });
     } catch (err) {
-        console.log(err)
+        console.log(err);
         res.status(500).json({
             success: false,
             message: 'Internal server error'
-        })
+        });
     }
-})
+});
 
 // @route POST api/auth/register
 // @desc register user
 // @access public
 router.post('/register', async (req, res) => {
-    const { username, password } = req.body
+    const { username, password } = req.body;
 
     if (!username || !password) {
         return res.status(400).json({
             success: false,
             message: 'Missing username or password'
-        })
+        });
     }
 
     try {
         // check username exists??
-        const user = await User.findOne({ username })
+        const user = await User.findOne({ username });
         if (user) {
             return res.status(400).json({
                 success: false,
-                message: 'username is already in use'
-            })
+                message: 'Username is already in use'
+            });
         }
 
         // all good
-        const hashedPassword = await argon2.hash(password)
+        const hashedPassword = await argon2.hash(password);
         const newUser = new User({
             username,
-            password: hashedPassword,
-        })
-        await newUser.save()
+            password: hashedPassword
+        });
+        await newUser.save();
 
         // return token
         // const accessToken = jwt.sign({ userId: newUser._id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: process.env.EXPIRES_TIME })
-        const accessToken = jwt.sign({ userId: newUser._id }, process.env.ACCESS_TOKEN_SECRET)
+        const accessToken = jwt.sign(
+            { userId: newUser._id },
+            process.env.ACCESS_TOKEN_SECRET
+        );
 
         return res.send({
             success: true,
-            message: 'resgister successfully',
-            accessToken,
-        })
+            message: 'Resgister successfully',
+            accessToken
+        });
     } catch (err) {
-        console.log(err)
+        console.log(err);
         res.status(500).json({
             success: false,
             message: 'Internal server error'
-        })
+        });
     }
-})
+});
 
 // @route POST api/auth/login
 // @desc login user
 // @access public
 router.post('/login', async (req, res) => {
-    const { username, password } = req.body
+    const { username, password } = req.body;
     if (!username || !password) {
         return res.status(400).json({
             success: false,
             message: 'Missing username or password'
-        })
+        });
     }
     try {
-        const user = await User.findOne({ username })
+        const user = await User.findOne({ username });
         if (!user) {
             return res.status(400).json({
                 success: false,
-                message: 'username or password is incorrect'
-            })
+                message: 'Username or password is incorrect'
+            });
         }
-        const passwordValid = await argon2.verify(user.password, password)
+        const passwordValid = await argon2.verify(user.password, password);
         if (!passwordValid) {
             return res.status(400).json({
                 success: false,
-                message: 'username or password is incorrect'
-            })
+                message: 'Username or password is incorrect'
+            });
         }
-        const accessToken = jwt.sign({ userId: user._id }, process.env.ACCESS_TOKEN_SECRET)
+        const accessToken = jwt.sign(
+            { userId: user._id },
+            process.env.ACCESS_TOKEN_SECRET
+        );
 
         return res.send({
             success: true,
-            message: 'login successfully',
-            accessToken,
-        })
+            message: 'Login successfully',
+            accessToken
+        });
     } catch (err) {
-        console.log(err)
+        console.log(err);
         res.status(500).json({
             success: false,
             message: 'Internal server error'
-        })
+        });
     }
-})
+});
 
-module.exports = router
+module.exports = router;
--- server/routes/post.js
+++ server/routes/post.js
@@ -1,107 +1,113 @@
-const express = require('express')
-const Post = require('../models/Post')
-const verifyToken = require('../middleware/auth')
+const express = require('express');
+const Post = require('../models/Post');
+const verifyToken = require('../middleware/auth');
 
-const router = express.Router()
+const router = express.Router();
 
 // @route POST api/posts
 // @desc get post
 // @access private
 router.get('/', verifyToken, async (req, res) => {
     try {
-        const posts = await Post.find({ user: req.userId }).populate('user', ['username'])
+        const posts = await Post.find({ user: req.userId }).populate('user', [
+            'username'
+        ]);
         res.json({
             success: true,
             posts
-        })
+        });
     } catch (err) {
-        console.log(err)
+        console.log(err);
         res.status(500).json({
             success: false,
             message: 'Internal server error'
-        })
+        });
     }
-})
+});
 
 // @route POST api/posts/create
 // @desc create post
 // @access private
 router.post('/create', verifyToken, async (req, res) => {
-    const { title, description, url, status } = req.body
+    const { title, description, url, status } = req.body;
     if (!title) {
         return res.status(400).json({
             success: false,
-            message: 'title is required'
-        })
+            message: 'Title is required'
+        });
     }
     try {
         const newPost = new Post({
             title,
             description,
-            url: (url.startsWith('https://')) ? url : `https://${url}`,
+            url: url.startsWith('https://') ? url : `https://${url}`,
             status: status || 'TO LEARN',
             user: req.userId
-        })
-        await newPost.save()
+        });
+        await newPost.save();
 
         res.send({
             success: true,
-            message: 'happy learning',
+            message: 'Happy learning',
             post: newPost
-        })
+        });
     } catch (err) {
-        console.log(err)
+        console.log(err);
         res.status(500).json({
             success: false,
             message: 'Internal server error'
-        })
+        });
     }
-})
+});
 
 // @route PUT api/posts/edit/:id
 // @desc update post
 // @access private
 router.put('/edit/:id', verifyToken, async (req, res) => {
-    const { title, description, url, status } = req.body
+    const { title, description, url, status } = req.body;
     if (!title) {
         return res.status(400).json({
             success: false,
-            message: 'title is required'
-        })
+            message: 'Title is required'
+        });
     }
     try {
         let updatedPost = {
             title,
             description: description || '',
-            url: ((url.startsWith('https://')) ? url : `https://${url}`) || '',
+            url: (url.startsWith('https://') ? url : `https://${url}`) || '',
             status: status || 'TO LEARN'
-        }
+        };
 
         const postUpdateCondition = {
             _id: req.params.id,
             user: req.userId
-        }
-        updatedPost = await Post.findOneAndUpdate(postUpdateCondition, updatedPost, { new: true })
+        };
+        updatedPost = await Post.findOneAndUpdate(
+            postUpdateCondition,
+            updatedPost,
+            { new: true }
+        );
         if (!updatedPost) {
             return res.status(401).json({
                 success: false,
-                message: 'post not found or user not authorized'
-            })
+                message: 'Post not found or user not authorized'
+            });
         }
 
         res.json({
             success: true,
-            message: 'excellent progress!',
+            message: 'Excellent progress!',
             post: updatedPost
-        })
+        });
     } catch (err) {
-        console.log(err)
+        console.log(err);
         res.status(500).json({
             success: false,
             message: 'Internal server error'
-        })
+        });
     }
-})
+});
 
 // @route PUT api/posts/delete/:id
 // @desc delete post
@@ -110,27 +116,27 @@ router.delete('/delete/:id', verifyToken, async (req, res) => {
     const postDeleteCondition = {
         _id: req.params.id,
         user: req.userId
-    }
+    };
     try {
-        const deletedPost = await Post.findOneAndDelete(postDeleteCondition)
+        const deletedPost = await Post.findOneAndDelete(postDeleteCondition);
         if (!deletedPost) {
             return res.status(401).json({
                 success: false,
-                message: 'post not found or user not authorized'
-            })
+                message: 'Post not found or user not authorized'
+            });
         }
         res.json({
             success: true,
-            message: 'delete post successfully',
+            message: 'Delete post successfully',
             post: deletedPost
-        })
+        });
     } catch (err) {
-        console.log(err)
+        console.log(err);
         res.status(500).json({
             success: false,
             message: 'Internal server error'
-        })
+        });
     }
-})
+});
 
-module.exports = router
+module.exports = router;
