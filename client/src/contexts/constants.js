69876db4472d9add2d64c1113c0deb30623f86de
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-01-17 18:09:27 +0700

    add constant for Post loaded

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -3,3 +3,6 @@ export const apiUrl =
         ? 'http://localhost:5000/api'
         : 'deployUrl';
 export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mern';
+
+export const POSTS_LOADED_SUCCESS = 'POSTS_LOADED_SUCCESS';
+export const POSTS_LOADED_FAIL = 'POSTS_LOADED_FAIL';
