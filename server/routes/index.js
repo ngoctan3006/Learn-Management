bde732114cc932056d2293ab020037ba3308624c
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-01-17 21:52:44 +0700

    add homepage for server

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,12 @@ const postRouter = require('./post');
 const route = (app) => {
     app.use('/api/auth', authRouter);
     app.use('/api/posts', postRouter);
+
+    app.use('/', (req, res) => {
+        res.send(
+            "<div style='display:flex;justify-content:center;align-items:center;height:100vh;color:red;font-size:5rem'>Hi, I'm Tân!! ❤️❤️❤️</div>"
+        );
+    });
 };
 
 module.exports = route;
